2d809060933989078c0d83cb1b84c1d75356640b
FaneSpoitoru <stefanologeanu2009@yahoo.com>
2026-04-26 11:31:57 +0300

    eroare vercel litere mici

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
-import MainPage from './pages/MainPage';
-import Contact from './pages/Contact';
+
+// AICI ESTE SECRETUL: calea din interiorul ghilimelelor TREBUIE să fie cu litere mici, 
+// exact cum sunt denumite fișierele tale în folderul pages!
+import MainPage from './pages/mainpage'; 
+import Contact from './pages/contact';   
 import Lectii from './pages/Lectii';
 import LessonPage from './pages/LessonPage';
-import Intro from './pages/introlectii'; // Presupun că așa se numește fișierul tău
+import Intro from './pages/introlectii';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/contact" element={<Contact />} />
-      
-      {/* Pagina de "Despre" / Intro - am simplificat ruta la /despre */}
       <Route path="/despre" element={<Intro />} />
-      
-      {/* Pagina cu TOATE lecțiile */}
       <Route path="/lectii" element={<Lectii />} />
-      
-      {/* Pagina dinamică pentru O lecție */}
       <Route path="/lectie/:idLectie" element={<LessonPage />} />
     </Routes>
   );
