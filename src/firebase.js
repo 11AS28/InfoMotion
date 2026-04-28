8b20aef2b5a1efbab696a79365874dd6b567c865
antidate723 <lapinfoo@gmail.com>
2026-04-28 18:22:46 +0300

    incearca acu fane

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // Am pus valorile false direct aici, ca string-uri (între ghilimele)
-  apiKey: "AIzaSyA-FakeApiKeyPentruDezvoltareLocala123",
-  authDomain: "proiect-test-local.firebaseapp.com",
-  projectId: "proiect-test-local",
-  storageBucket: "proiect-test-local.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890abcdef"
+  // Schimbă process.env cu import.meta.env
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
