ee795d8c40a69b92f4e450873d99aa6cbc943188
antidate723 <lapinfoo@gmail.com>
2026-06-14 13:37:56 +0300

    da ma??

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cors({
 }));         
 app.use(express.json());  
 
-app.post('/api/simulate', async (req, res) => {
+app.post('api/simulate', async (req, res) => {
   try {
     const { algorithmType, inputData } = req.body;
     
