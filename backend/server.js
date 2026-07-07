60a62c18b3ba449f57f5a7fd4b180282058ec2c1
antidate723 <lapinfoo@gmail.com>
2026-07-07 17:17:44 +0300

    domain nou

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['https://info-motion.vercel.app', 'http://localhost:3000'],
+  origin: ['https://infomotion.space', 'http://localhost:3000'],
   methods: ['POST', 'GET', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
